f034d296b2e051858b04eb91b71762a0e662728c
Current7777 <76453016+Current7777@users.noreply.github.com>
2021-11-21 00:17:02 +0600

    core

---
+++ up_SCBomber.sh
@@ -0,0 +1,94 @@
+##########################################################
+# DEV : Current77777
+# Github : https://github.com/Current7777
+# I am completely destroyed. 304 Not Modified.,,
+
+# THANKS TO ALLAH
+
+####################<@>=========Current=========<@>################
+
+
+
+
+
+
+
+
+
+a='\033[1;30m'
+rc='\033[1;31m'
+g='\033[1;32m' 
+y='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+c='\033[1;36m' 
+w='\033[1;37m'
+m='\033[1;94m'
+n='\033[0;00m' 
+
+logo(){
+  clear
+  echo -e "${y}  ____  ${c}  ____ ${g}    ____   ___  __  __ ${g}SCBOMB${n}  "
+  echo -e "${y} / ___| ${c} / ___| ${g}  | __ ) / _ \|  \/  | __ ) "
+  echo -e "${y} \___ \ ${c}| |     ${g}  |  _ \| | | | |\/| |  _ \ "
+  echo -e "${y}  ___) |${c}| |___ ${g}   | |_) | |_| | |  | | |_) |"
+  echo -e "${y} |____/ ${c} \____|${g}___|____/ \___/|_|  |_|____/ "
+  echo -e "                      |_${p}Current${g}_|                        "
+echo 
+echo 
+echo
+
+
+echo -e "                  \033[1;30m[ \033[1;34mAUTHER \033[1;30m] \033[1;32mCURRENT \033[1;32mHACKER\033[1;31m 7777"
+echo -e "                  \033[1;30m[\033[1;34m GITHUB\033[1;30m ] \033[1;34mCurrent7777"
+echo -e "                  \033[1;30m[ \033[1;34mCODER \033[1;30m ] \033[1;30mCurrent7777 "
+
+}
+logo
+update(){
+  echo -e "${p} A UPDATE AVAILABLE"
+  sleep 0.1
+  echo -e "${m} SCBOMB IS UPDATING"
+  sleep 1
+  xdg-open https://www.facebook.com/groups/242589267650518/?ref=share'
+  echo -e "${b} PLEASE WAIT..." 
+  cd ; 
+  rm -rf SCBomber ; 
+  apt update ; 
+  apt install python -y ; 
+  apt install curl -y ; 
+  pip install requests ; 
+  curl https://codeload.github.com/Current7777/SCBomber/zip/refs/heads/main --output SCBomber.zip ; 
+  unzip SCBomber.zip ; 
+  rm -rf SCBomber.zip ; 
+  mv SCBomber
+
+  echo
+  echo -e "${g} NOW YOUR TOOL UPDATED."
+  echo
+  echo -e "${y} THANKS FOR UPDATE ME."
+  echo
+  echo -e "${g}<==> NOW TYPE <==>"
+  echo
+  echo -e "${b} cd ; cd SCBomber ; python SCBomber.py${n}" ;
+  sleep 3
+
+
+}
+update
+echo 
+echo
+exit
+
+
+
+
+
+##########################################################
+# DEV : Current7777
+# Github : https://github.com/Current7777
+
+# THANKS TO ALLAH
+# WELCOME HOME
+# Current7777
+#####################<@>=========Current=========<@>################
